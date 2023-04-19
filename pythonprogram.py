6bdbb05f0d7f5e4886bbf99781d0cb06135f291d
Ravindrakumar05 <palleravindrakumar@gmail.com>
2023-04-19 10:21:37 +0530

    first commit

---
+++ pythonprogram.py
@@ -0,0 +1,8 @@
+#program to find the given number is zero or even or odd 
+num = int(input("Enter any number")) 
+if (num==0):
+    print("The given number is zero")
+elif (num%2==0):
+    print("The given number is even number")
+else:
+    print("The given number is odd number")
